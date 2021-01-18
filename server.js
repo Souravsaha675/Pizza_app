df12b9de9d4e3a45a2d0f5bbc0f6b64f6f124e13
sourav saha <souravsaha675@gmail.com>
2021-01-18 23:28:56 +0530

    Order Page

--- server.js
+++ server.js
@@ -11,8 +11,6 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.static("public"));
 
-app.get("/", (req, res) => res.render("home"));
-
 //Set Template Engine
 
 app.use(expressLayout);
@@ -20,6 +18,12 @@ app.use(expressLayout);
 app.set("views", path.join(__dirname, "/resources/views"));
 
 app.set("view engine", "ejs");
+
+//Routes
+
+app.get("/", (req, res) => res.render("home"));
+
+app.get("/cart", (req, res) => res.render("customers/cart"));
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}.`);
